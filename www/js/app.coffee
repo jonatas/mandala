efa7c71e7d47de139bdb8cefc2ca19ed5267b51b
Jônatas Paganini <jonatasdp@gmail.com>
2014-07-04 19:20:35 -0300

    fixing back to work on the main scope with angular. using independent structure from device ready. making noise with timbre.js to simulate the motor noise :+1:

--- www/js/app.coffee
+++ www/js/app.coffee
@@ -1,64 +1,74 @@
 window.mandalas =
-  initialize: -> @bindEvents()
+  initialize: ->
+    console.log("initialize binding events...")
+    @bindEvents()
   bindEvents: ->
     document.addEventListener('deviceready', this.onDeviceReady, false)
+    console.log("events binded!")
   onDeviceReady: ->
-    window.app = angular.module("mandala-app", [])
-    window.main = ($scope) ->
-      setAnimationState = (image, state) ->
-        image.style.webkitAnimationPlayState =
-          image.style.mozAnimationPlayState =
-            image.style.oAnimationPlayState =
-              image.style.animationPlayState = state
+    console.log("Ok! device ready!" )
 
-      setAnimation = (image, actualAnimation) ->
-        image.style.webkitAnimation =
-          image.style.mozAnimation =
-            image.style.oAnimation =
-              image.style.animation = actualAnimation
+window.app = angular.module("mandala-app", [])
+window.main = ($scope) ->
 
+  setAnimationState = (image, state) ->
+    if image isnt null
+      image.style.webkitAnimationPlayState = image.style.mozAnimationPlayState = image.style.oAnimationPlayState = image.style.animationPlayState = state
 
-      $scope.livros_a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-      $scope.livros_b = [1,2,3,4,5,6,7,8,9]
-      $scope.fromRPM = -> (60.0 / $scope.accelerator )
-      $scope.mandalas =[]
-      for livro in $scope.livros_a
-        $scope.mandalas.push("mandalas/1/500x500/0#{livro}b.png")
-      for livro in $scope.livros_b
-        $scope.mandalas.push("mandalas/2/500x500/0#{livro}b.png")
+  setAnimation = (image, actualAnimation) ->
+    if image isnt null
+      image.style.webkitAnimation = image.style.mozAnimation = image.style.oAnimation = image.style.animation = actualAnimation
 
-      $scope.img_mandalas = ->
-        document.querySelector("img[src='#{mandala}']") for mandala in $scope.mandalas
+  $scope.livros_a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  $scope.livros_b = [1,2,3,4,5,6,7,8,9]
+  $scope.fromRPM = -> (60.0 / $scope.accelerator )
+  $scope.mandalas =[]
+  for livro in $scope.livros_a
+    $scope.mandalas.push("mandalas/1/500x500/0#{livro}b.png")
+  for livro in $scope.livros_b
+    $scope.mandalas.push("mandalas/2/500x500/0#{livro}b.png")
+
+  $scope.img_mandalas = ->
+    document.querySelector("img[src='#{mandala}']") for mandala in $scope.mandalas
 
 
-      $scope.velocimeter = -> "#{$scope.accelerator} RPM"
-      $scope.accelerate = ->
-        for mandala in $scope.img_mandalas()
-          setAnimation(mandala, "")
+  $scope.velocimeter = -> "#{$scope.accelerator} RPM"
+  $scope.accelerate = ->
+    newFrequency = {freq: parseInt($scope.accelerator)}
+    console.log("new Frequency: "+newFrequency)
+    $scope.synth.set(newFrequency)
+    $scope.synth.play()
+    for mandala in $scope.img_mandalas()
+      setAnimation(mandala, "") if mandala !=null
 
-        newAnimation = ->
-          actualAnimation = "rotation #{ $scope.fromRPM()}s infinite linear"
-          for mandala in $scope.img_mandalas()
-            setAnimation(mandala, actualAnimation)
-        setTimeout(newAnimation, 200)
+    newAnimation = ->
+      actualAnimation = "rotation #{ $scope.fromRPM()}s infinite linear"
+      for mandala in $scope.img_mandalas()
+        setAnimation(mandala, actualAnimation)
+    setTimeout(newAnimation, 200)
 
-        if not $scope.turn_on_motor
-          $scope.turn_on_motor = true
+    if not $scope.turn_on_motor
+      $scope.turn_on_motor = true
 
 
-      $scope.showOriginal = (element) ->
-        element.mandala = element.mandala.replace("b.png", ".png")
-      $scope.showInCircle = (element) ->
-        element.mandala = element.mandala.replace(".png", "b.png")
+  $scope.showOriginal = (element) ->
+    element.mandala = element.mandala.replace("b.png", ".png")
+  $scope.showInCircle = (element) ->
+    element.mandala = element.mandala.replace(".png", "b.png")
 
-      $scope.switch_on_off = ->
-        if $scope.turn_on_motor
-          state = "running"
-        else
-          state = "paused"
+  $scope.switch_on_off = ->
+    if $scope.turn_on_motor
+      state = "running"
+      $scope.synth.play()
+    else
+      state = "paused"
+      $scope.synth.pause()
 
-        for image in $scope.img_mandalas()
-          setAnimationState(image, state)
 
-      $scope.turn_on_motor = false
-      $scope.accelerator = 0
+    for image in $scope.img_mandalas()
+      setAnimationState(image, state)
+
+  $scope.turn_on_motor = false
+  $scope.accelerator = 0
+
+  $scope.synth = T("sin")
--- www/js/app.js
+++ www/js/app.js
@@ -2,98 +2,118 @@
 (function() {
   window.mandalas = {
     initialize: function() {
+      console.log("initialize binding events...");
       return this.bindEvents();
     },
     bindEvents: function() {
-      return document.addEventListener('deviceready', this.onDeviceReady, false);
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+      return console.log("events binded!");
     },
     onDeviceReady: function() {
-      window.app = angular.module("mandala-app", []);
-      return window.main = function($scope) {
-        var livro, setAnimation, setAnimationState, _i, _j, _len, _len1, _ref, _ref1;
-        setAnimationState = function(image, state) {
-          return image.style.webkitAnimationPlayState = image.style.mozAnimationPlayState = image.style.oAnimationPlayState = image.style.animationPlayState = state;
-        };
-        setAnimation = function(image, actualAnimation) {
-          return image.style.webkitAnimation = image.style.mozAnimation = image.style.oAnimation = image.style.animation = actualAnimation;
-        };
-        $scope.livros_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-        $scope.livros_b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        $scope.fromRPM = function() {
-          return 60.0 / $scope.accelerator;
-        };
-        $scope.mandalas = [];
-        _ref = $scope.livros_a;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          livro = _ref[_i];
-          $scope.mandalas.push("mandalas/1/500x500/0" + livro + "b.png");
-        }
-        _ref1 = $scope.livros_b;
-        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-          livro = _ref1[_j];
-          $scope.mandalas.push("mandalas/2/500x500/0" + livro + "b.png");
-        }
-        $scope.img_mandalas = function() {
-          var mandala, _k, _len2, _ref2, _results;
-          _ref2 = $scope.mandalas;
-          _results = [];
-          for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
-            mandala = _ref2[_k];
-            _results.push(document.querySelector("img[src='" + mandala + "']"));
-          }
-          return _results;
-        };
-        $scope.velocimeter = function() {
-          return "" + $scope.accelerator + " RPM";
-        };
-        $scope.accelerate = function() {
-          var mandala, newAnimation, _k, _len2, _ref2;
-          _ref2 = $scope.img_mandalas();
-          for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
-            mandala = _ref2[_k];
-            setAnimation(mandala, "");
-          }
-          newAnimation = function() {
-            var actualAnimation, _l, _len3, _ref3, _results;
-            actualAnimation = "rotation " + ($scope.fromRPM()) + "s infinite linear";
-            _ref3 = $scope.img_mandalas();
-            _results = [];
-            for (_l = 0, _len3 = _ref3.length; _l < _len3; _l++) {
-              mandala = _ref3[_l];
-              _results.push(setAnimation(mandala, actualAnimation));
-            }
-            return _results;
-          };
-          setTimeout(newAnimation, 200);
-          if (!$scope.turn_on_motor) {
-            return $scope.turn_on_motor = true;
-          }
-        };
-        $scope.showOriginal = function(element) {
-          return element.mandala = element.mandala.replace("b.png", ".png");
-        };
-        $scope.showInCircle = function(element) {
-          return element.mandala = element.mandala.replace(".png", "b.png");
-        };
-        $scope.switch_on_off = function() {
-          var image, state, _k, _len2, _ref2, _results;
-          if ($scope.turn_on_motor) {
-            state = "running";
-          } else {
-            state = "paused";
-          }
-          _ref2 = $scope.img_mandalas();
-          _results = [];
-          for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
-            image = _ref2[_k];
-            _results.push(setAnimationState(image, state));
-          }
-          return _results;
-        };
-        $scope.turn_on_motor = false;
-        return $scope.accelerator = 0;
-      };
+      return console.log("Ok! device ready!");
     }
+  };
+
+  window.app = angular.module("mandala-app", []);
+
+  window.main = function($scope) {
+    var livro, setAnimation, setAnimationState, _i, _j, _len, _len1, _ref, _ref1;
+    setAnimationState = function(image, state) {
+      if (image !== null) {
+        return image.style.webkitAnimationPlayState = image.style.mozAnimationPlayState = image.style.oAnimationPlayState = image.style.animationPlayState = state;
+      }
+    };
+    setAnimation = function(image, actualAnimation) {
+      if (image !== null) {
+        return image.style.webkitAnimation = image.style.mozAnimation = image.style.oAnimation = image.style.animation = actualAnimation;
+      }
+    };
+    $scope.livros_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+    $scope.livros_b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    $scope.fromRPM = function() {
+      return 60.0 / $scope.accelerator;
+    };
+    $scope.mandalas = [];
+    _ref = $scope.livros_a;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      livro = _ref[_i];
+      $scope.mandalas.push("mandalas/1/500x500/0" + livro + "b.png");
+    }
+    _ref1 = $scope.livros_b;
+    for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+      livro = _ref1[_j];
+      $scope.mandalas.push("mandalas/2/500x500/0" + livro + "b.png");
+    }
+    $scope.img_mandalas = function() {
+      var mandala, _k, _len2, _ref2, _results;
+      _ref2 = $scope.mandalas;
+      _results = [];
+      for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
+        mandala = _ref2[_k];
+        _results.push(document.querySelector("img[src='" + mandala + "']"));
+      }
+      return _results;
+    };
+    $scope.velocimeter = function() {
+      return "" + $scope.accelerator + " RPM";
+    };
+    $scope.accelerate = function() {
+      var mandala, newAnimation, newFrequency, _k, _len2, _ref2;
+      newFrequency = {
+        freq: parseInt($scope.accelerator)
+      };
+      console.log("new Frequency: " + newFrequency);
+      $scope.synth.set(newFrequency);
+      $scope.synth.play();
+      _ref2 = $scope.img_mandalas();
+      for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
+        mandala = _ref2[_k];
+        if (mandala !== null) {
+          setAnimation(mandala, "");
+        }
+      }
+      newAnimation = function() {
+        var actualAnimation, _l, _len3, _ref3, _results;
+        actualAnimation = "rotation " + ($scope.fromRPM()) + "s infinite linear";
+        _ref3 = $scope.img_mandalas();
+        _results = [];
+        for (_l = 0, _len3 = _ref3.length; _l < _len3; _l++) {
+          mandala = _ref3[_l];
+          _results.push(setAnimation(mandala, actualAnimation));
+        }
+        return _results;
+      };
+      setTimeout(newAnimation, 200);
+      if (!$scope.turn_on_motor) {
+        return $scope.turn_on_motor = true;
+      }
+    };
+    $scope.showOriginal = function(element) {
+      return element.mandala = element.mandala.replace("b.png", ".png");
+    };
+    $scope.showInCircle = function(element) {
+      return element.mandala = element.mandala.replace(".png", "b.png");
+    };
+    $scope.switch_on_off = function() {
+      var image, state, _k, _len2, _ref2, _results;
+      if ($scope.turn_on_motor) {
+        state = "running";
+        $scope.synth.play();
+      } else {
+        state = "paused";
+        $scope.synth.pause();
+      }
+      _ref2 = $scope.img_mandalas();
+      _results = [];
+      for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
+        image = _ref2[_k];
+        _results.push(setAnimationState(image, state));
+      }
+      return _results;
+    };
+    $scope.turn_on_motor = false;
+    $scope.accelerator = 0;
+    return $scope.synth = T("sin");
   };
 
 }).call(this);
